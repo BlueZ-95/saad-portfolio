a074bd18308f78bcf176d86cf0543210b2a01956
Sa'ad <62077144+BlueZ-95@users.noreply.github.com>
2021-04-01 22:19:13 +0530

    sync

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://github.com/BlueZ-95">Saad!</a>
+          Portfolio Coming Soon!
         </h1>
       </main>
     </div>
