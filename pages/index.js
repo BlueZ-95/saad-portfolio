9d06d4ec5f21dc72f59ba9a0d3978f77df2fe87a
Sa'ad <62077144+BlueZ-95@users.noreply.github.com>
2021-04-01 22:25:30 +0530

    sync

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Saad Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Portfolio Coming Soon!
+          <a href='https://github.com/BlueZ-95'>Portfolio</a> Coming Soon!
         </h1>
       </main>
     </div>
