9f8e05a59cb2c3576c5abb7f6f89bc56d2cf5b4d
Saad <saad.patel@thinksense.ai>
2021-04-02 19:30:43 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -11,18 +11,20 @@ export default function Home() {
 
       <header className={`${styles.headerContainer} container-fluid`}>
         <div className='row h-100'>
-          <div className='col-sm-6 flex-column'>
-            <div className={`${styles.menuContainer} col d-flex align-items-center justify-content-between`}>
-              <h1>me.</h1>
+          <div className='col-sm-5 flex-column p-0'>
+            <div className={`${styles.menuContainer} col h-25 d-flex align-items-start justify-content-between p-4`}>
+              <h2 className="fw-bold">me.</h2>
               <h3>menu</h3>
             </div>
-            <div className={`${styles.headerLeftContent} `}></div>
+            <div className={`${styles.headerLeftContent} col h-75 d-flex flex-column align-items-center justify-content-center`}>
+              <h1 className="fs-1 fw-bold">SAAD <span className="fs-5 fw-light">Patel</span></h1>
+            </div>
           </div>
-          <div className='col-sm-6 flex-column'>
-            <div className='h-75'>Right-Top</div>
-            <div className=''>
-              <div className='col-sm-6 d-inline-block'>Right-Bottom-Left</div>
-              <div className='col-sm-6 d-inline-block'>Right-Bottom-Right</div>
+          <div className='col-sm-7 flex-column p-0'>
+            <div className='h-75 bg-info'>Right-Top</div>
+            <div className='h-25'>
+              <div className='h-100 col-sm-6 d-inline-block bg-secondary'>Right-Bottom-Left</div>
+              <div className='h-100 col-sm-6 d-inline-block bg-dark'>Right-Bottom-Right</div>
             </div>
           </div>
         </div>
