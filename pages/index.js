e0d60d72c2c0667c4db1bf830274f0467ad58651
Saad <saad.patel@thinksense.ai>
2021-04-03 15:57:39 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { FaInstagram, FaTwitter, FaLinkedinIn, FaGithub } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -11,13 +12,19 @@ export default function Home() {
 
       <header className={`${styles.headerContainer} container-fluid`}>
         <div className='row h-100'>
-          <div className='col-sm-5 flex-column p-0'>
-            <div className={`${styles.menuContainer} col h-25 d-flex align-items-start justify-content-between p-4`}>
+          <div className='col-sm-5 flex-column p-0' style={{ maxHeight: '100vh' }}>
+            <div className={`${styles.menuContainer} col d-flex align-items-start justify-content-between p-4`}>
               <h2 className="fw-bold">me.</h2>
               <h3>menu</h3>
             </div>
-            <div className={`${styles.headerLeftContent} col h-75 d-flex flex-column align-items-center justify-content-center`}>
-              <h1 className="fs-1 fw-bold">SAAD <span className="fs-5 fw-light">Patel</span></h1>
+            <div className={`${styles.headerLeftContent} col d-flex flex-column align-items-center justify-content-center`}>
+              <h1 className="fs-1 fw-bold">SAAD</h1><p>Developer based in Vadodara</p>
+            </div>
+            <div className={`${styles.socialMediaLinks} col fs-4 d-flex align-items-center justify-content-evenly`}>
+              <FaInstagram></FaInstagram>
+              <FaTwitter></FaTwitter>
+              <FaLinkedinIn></FaLinkedinIn>
+              <FaGithub></FaGithub>
             </div>
           </div>
           <div className='col-sm-7 flex-column p-0'>
