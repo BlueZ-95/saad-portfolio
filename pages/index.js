4f7c4f72be9fc6cfff42570404e33a03af7069e8
Saad <saad.patel@thinksense.ai>
2021-04-06 18:32:02 +0530

    added header

--- pages/index.js
+++ pages/index.js
@@ -11,26 +11,37 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className={`${styles.headerContainer} container-fluid`}>
-        <div className='row h-100'>
-          <div className='col-sm-5 flex-column p-0' style={{ maxHeight: '95vh' }}>
-            <div className={`${styles.menuContainer} col d-flex align-items-start justify-content-between p-4`}>
-              <h2 className="fw-bold">me.</h2>
-              <h3>menu</h3>
+      <header className={`${styles.headerContainer}`}>
+        <div className="container-fluid h-100">
+          <div className='row h-25 d-flex align-items-center'>
+            <div className='col-sm-8 h-50'>
+              <ul className='col-sm-10 offset-sm-2 d-flex'>
+                <li className='col-sm-1'>Menu</li>
+                <li className='col-sm-1'>About</li>
+                <li className='col-sm-1'>Contact</li>
+              </ul>
             </div>
-            <div className={`${styles.headerLeftContent} col d-flex flex-column align-items-center justify-content-center`}>
-              <h1 className="fs-1">I'm <b>SAAD</b></h1><p>Developer based in Vadodara</p>
-            </div>
-            <div className={`${styles.socialMediaLinks} col fs-4 d-flex align-items-center justify-content-evenly`}>
-              <FaInstagram></FaInstagram>
-              <FaTwitter></FaTwitter>
-              <FaLinkedinIn></FaLinkedinIn>
-              <FaGithub></FaGithub>
+            <div className='col-sm-4 h-50'>
+              <ul className='d-flex align-items-center justify-content-end'>
+                <li className='col-sm-2'>a</li>
+                <li className='col-sm-2'>b</li>
+                <li className='col-sm-2'>c</li>
+              </ul>
             </div>
           </div>
-          <div className='col-sm-7 flex-column p-0'>
-            <div className='h-100'>
-
+          <div className='row h-75 pt-5'>
+            <div className='col-sm-6 offset-sm-1 d-flex align-items-start justify-content-center'>
+              <div className='d-flex flex-column align-items-start justify-content-start pt-5'>
+                <div className='w-75'>
+                  <h4>Hey,</h4>
+                  <h1>I'm Saad</h1>
+                  <p className={`${styles.headerPersonalInfo} fs-5`}>Freelancer <b>Web Developer</b>, also passionate in making beautiful <b>UI/UX</b> designs.</p>
+                  <button className='btn btn-primary btn-custom w-50 mt-3'>Hire Me</button>
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-4 offset-sm-1 p-0'>
+              <div className={`${styles.avatarContainer} w-75 h-75 shadow-lg`}></div>
             </div>
           </div>
         </div>
